ac7603284e56a555d3d8548cec232c67e498c809
Noah Gift <noah.gift@gmail.com>
2022-01-03 13:45:05 -0500

    Adding bucket lister console app

---
+++ S3CreateAndList/S3CreateAndList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+// To interact with Amazon S3.
+using Amazon.S3;
+using Amazon.S3.Model;
+
+// Create an S3 client object.
+var s3Client = new AmazonS3Client();
+
+// Display Prompt
+Console.WriteLine("AWS Bucket Lister" + Environment.NewLine);
+
+// Process API Calls Async List AWS Buckets
+var listResponse = await MyListBucketsAsync(s3Client);
+Console.WriteLine($"Number of buckets: {listResponse.Buckets.Count}");
+
+// Loop through the AWS buckets
+foreach (S3Bucket b in listResponse.Buckets)
+{
+    Console.WriteLine(b.BucketName);
+}
+
+static async Task<ListBucketsResponse> MyListBucketsAsync(IAmazonS3 s3Client)
+{
+    return await s3Client.ListBucketsAsync();
+}
